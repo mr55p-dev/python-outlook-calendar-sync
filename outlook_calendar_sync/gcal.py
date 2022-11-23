a19f429d58c7ea709f5e30cae655bc46e0fc9d9c
Ellis Lunnon <ellis.lunnon@sainsburys.co.uk>
2022-11-23 12:28:53 +0000

    Initial commit of project
    There are some issues which still need working out and posting online

---
+++ outlook_calendar_sync/gcal.py
@@ -0,0 +1,86 @@
+import logging
+from pathlib import Path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+from outlook_calendar_sync.utils import compare_events
+
+app_log = logging.getLogger("application")
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def get_gcal_api():
+    # Setup google calendar integration
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if Path('./token.json').exists():
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('calendar', 'v3', credentials=creds)
+
+
+def find_new_outlook_events(outlook_events, gcal_events):
+    new_events = []
+    # Find all newly discovered events
+    for o_event in outlook_events:
+        event_in_gcal = False
+
+        for g_event in gcal_events:
+            if compare_events(g_event, o_event):  # events match
+                event_in_gcal = True
+                break
+
+        if not event_in_gcal:
+            new_events.append(o_event)
+
+    app_log.info("Found %d events to add", len(new_events))
+
+    return new_events
+
+
+def find_incorrect_gcal_events(outlook_events, gcal_events):
+    incorrect_events = []
+    # Remove any events not found in outlook
+    for g_event in gcal_events:
+        event_in_outlook = False
+
+        for o_event in outlook_events:
+            if compare_events(g_event, o_event):  #  Events match
+                event_in_outlook = True
+                break
+
+        if not event_in_outlook:
+            incorrect_events.append(g_event)
+
+    app_log.info("Found %d events to remove", len(incorrect_events))
+
+    return incorrect_events
+
+
+def update_calendar(service, calendar_id, events_to_add, events_to_remove):
+    # perform updates
+    for g_event in events_to_remove:
+        service.events().delete(calendarId=calendar_id,
+                                eventId=g_event['id']).execute()
+
+    for o_event in events_to_add:
+        service.events().insert(calendarId=calendar_id, body=o_event).execute()
---
+++ outlook_calendar_sync/outlook.py
@@ -0,0 +1,73 @@
+import logging
+from time import sleep
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from outlook_calendar_sync.utils import find_event, get_event
+
+app_log = logging.getLogger("application")
+
+
+def outlook_login(driver, username: str, password: str, auth_code=None):
+    calendar_uri = "https://outlook.office.com/calendar/view/day"
+    driver.get(calendar_uri)
+
+    driver.implicitly_wait(10)
+
+    username_field = driver.find_element(By.ID, "i0116")
+    signin_btn = driver.find_element(By.ID, "idSIButton9")
+
+    username_field.send_keys(username)
+    signin_btn.click()
+
+    password_btn = driver.find_element(By.ID, "passwordInput")
+    signin_btn_auth = driver.find_element(By.ID, "submitButton")
+
+    password_btn.send_keys(password)
+    signin_btn_auth.click()
+
+    # Handle 2fa
+    auth_code_field = driver.find_element(By.ID, "idTxtBx_SAOTCC_OTC")
+    auth_code_submit = driver.find_element(By.ID, "idSubmit_SAOTCC_Continue")
+
+    if not auth_code:
+        auth_code = input("Provide the authenticator code: ")
+
+    auth_code_field.send_keys(auth_code)
+    auth_code_submit.click()
+
+    stay_signed_in_btn = driver.find_element(By.ID, "idBtn_Back")
+    stay_signed_in_btn.click()
+
+    return driver
+
+
+def outlook_select_page_events(driver, days=1, delay=5):
+    sleep(delay)
+    soup = BeautifulSoup(driver.page_source, features="html.parser")
+    yield [get_event(i["aria-label"]) for i in soup.find_all(find_event)]
+
+    for _ in range(days-1):
+        next_page = driver.find_element(
+            By.XPATH, '//*[@id="MainModule"]/div[3]/div/div[1]/div[1]/button[3]')
+        next_page.click()
+        sleep(delay)
+
+        # Dump the page source into BS
+        soup = BeautifulSoup(driver.page_source, features="html.parser")
+        yield [get_event(i["aria-label"]) for i in soup.find_all(find_event)]
+
+        # Get the next page
+
+    driver.quit()
+
+
+def get_selenium_driver(debug=False):
+    chrome_opt = Options()
+    if not debug:
+        chrome_opt.add_argument("--headless")
+    chrome_opt.add_argument("executable_path=/usr/local/bin/chromedriver")
+    return webdriver.Chrome(options=chrome_opt)
